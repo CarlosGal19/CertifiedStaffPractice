0d9279f7797f6c0087ebafda0b470654a3d88449
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-28 17:45:28 -0600

    fix: set timestamps as optional

--- Models/Certification.cs
+++ Models/Certification.cs
@@ -8,8 +8,8 @@ public class Certification
     public int ProductionLineStationId {get; set;}
     public ProductionLineStation ProductionLineStation { get; set; } = null!;
     public int TrainingPercentage {get; set;}
-    public DateTime CertificationDate {get; set;}
-    public DateTime ExpirationDate {get; set;}
+    public DateTime? CertificationDate {get; set;}
+    public DateTime? ExpirationDate {get; set;}
     public bool IsActive {get; set;} = true;
     public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
 }
