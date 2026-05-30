6fe2e0c6644c1256a2f1e905a2373330980b1294
Carlos Galindo <c.galindo.islas@gmail.com>
2026-05-30 15:32:21 -0600

    feat: create dto to get certificate to update

---
+++ DTO/Update/GetCertificationDTO.cs
@@ -0,0 +1,10 @@
+namespace CertifiedStaff.DTO.Update;
+
+public class GetCertificationDTO
+{
+    public int CertificateId {get; set;}
+    public int EmployeeId {get; set;}
+    public int ProductionLineId {get; set;}
+    public int StationId {get; set;}
+    public int TrainingPercentage {get; set;}
+}
